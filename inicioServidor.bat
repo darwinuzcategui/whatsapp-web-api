750e5651d430efbc68a3f54eff7cc4f7cc708566
Darwin Uzcategui <darwin.uzcategui1973@gmail.com>
2025-08-05 14:01:54 -0400

    quitar archivo que no hacen falta

--- inicioServidor.bat
+++
@@ -1,7 +0,0 @@
-echo off
-echo Debe leer el codigo QR CON SU Whastapp^(s^):
-echo para salir de api de envio pulsar Crt+ c
-echo  para salir o Cerrar la ventana
-echo  ------------------------------------------
-echo off
-npx cross-env DEBUG=Whastapp-web-api:* npm start
